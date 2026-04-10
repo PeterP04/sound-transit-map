74409bc9578fce37a4eb0001162a15170c3e4bfa
PeterP04 <peterpham04@outlook.com>
2026-04-10 10:09:49 -0700

    Import Header component in App.jsx

--- sound-transit-map/src/App.jsx
+++ sound-transit-map/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Map from "./Map";
+import Header from "components./Header";
 
 function App() {
   return <Map />;
