cee87270f907eb901ee995d5c5842a20ac820935
PeterP04 <peterpham04@outlook.com>
2026-04-10 11:02:56 -0700

    Update App.jsx

--- sound-transit-map/src/App.jsx
+++ sound-transit-map/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Map from "./Map";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="mapContainer">
         <Map />
       </div>
+      <Footer />
     </div>
   );
 }
